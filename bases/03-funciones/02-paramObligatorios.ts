348c39cb08cae86d560fb49eb9c7debc6a6228e8
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 12:30:57 +0200

    03-parámetros obligatorios en funciones

---
+++ bases/03-funciones/02-paramObligatorios.ts
@@ -0,0 +1,26 @@
+(() => {
+  console.log('PARAMETROS OBLIGATORIOS DE LAS FUNCIONES');
+
+  // Indicamos que firstName y lastName son de tipo string porque si no TS nos indica que son de tipo any,
+  const fullName = (firstName: string, lastName: string): string => {
+    // Si estuviéramos trabajando en JS habría que hacer validaciones de los parámetros. Ver si vienen o no.
+    // CON TS NOS TIENEN QUE MANDAR LOS 2 STRINGS.
+    // De todas formas esto serviría porque TS no es a prueba de errores y siempre podemos mandar un firstName a undefined.
+    if (!firstName) {
+      throw new Error('Nombre requerido');
+    }
+
+    return `${firstName} ${lastName}`;
+  };
+
+  // Tal y como se ha definido la función, los parámetros son obligatorios. Si no mandamos 2 argumentos string, TS nos va a dar un error.
+  //const nameError = fullName('Tony');
+  const name = fullName('Tony', 'Startk');
+  console.log({ name });
+
+  // Ejemplo de firstName a undefined.
+  // TS me deja hacerlo.
+  // Esta es otra razón de por qué no es bueno usar any.
+  let noName: any;
+  const name2 = fullName(noName, 'Stark');
+})();
