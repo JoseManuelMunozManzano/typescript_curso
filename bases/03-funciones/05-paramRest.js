fed5941dd00a5efd3e98236198f021d1804e0112
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 13:02:19 +0200

    03-Parámetros rest

---
+++ bases/03-funciones/05-paramRest.js
@@ -0,0 +1,17 @@
+"use strict";
+(() => {
+    console.log('PARAMETROS RESTS');
+    // Solo firstName es obligatorio y todos los demás argumentos son opcionales y se pueden mandar cualquier
+    // cantidad de argumentos extra.
+    // Para definir una función que permita esto se utilizan los rest arguments (resto de argumentos)
+    // Esto lleva usándose mucho tiempo en JS.
+    // En TS se el tipo de dato de los rest Args es un array:
+    const fullName = (firstName, ...restArgs) => {
+        // Devuelve el firstName y la concatenación de todos los nombres mandados usando el join.
+        return `${firstName} ${restArgs.join(' ')}`;
+    };
+    // Solo el primer argumento es obligatorio. Luego se pueden mandar ninguno o todos los demás argumentos
+    // que queramos.
+    const superman = fullName('Clark', 'Joseph', 'Kent');
+    console.log({ superman });
+})();
