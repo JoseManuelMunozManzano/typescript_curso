4d618d40a2ea09fdd170e09433a97a1f862d0cc6
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 12:38:54 +0200

    03-parámetros opcionales de las funciones

---
+++ bases/03-funciones/03-paramOpcionales.ts
@@ -0,0 +1,14 @@
+(() => {
+  console.log('PARAMETROS OPCIONALES DE LAS FUNCIONES');
+
+  // Para indicar a TS que un parámetro es opcional, se usa ?
+  // En JS no hay manera de indicar que un parámetro es obligatorio u opcional. Todos son opcionales.
+  const fullName = (firstName: string, lastName?: string): string => {
+    // Indicando que si no tenemos lastName escriba -----
+    return `${firstName} ${lastName || '-----'}`;
+  };
+
+  // Ahora, al definir lasttName como opcional, puedo mandar solo un string.
+  const name = fullName('Tony');
+  console.log({ name });
+})();
