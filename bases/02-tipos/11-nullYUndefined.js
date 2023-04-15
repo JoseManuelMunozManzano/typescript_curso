171333ae2fd9ea9bba7a0489b4740752887bda47
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 11:39:29 +0200

    02-null y undefined

---
+++ bases/02-tipos/11-nullYUndefined.js
@@ -0,0 +1,25 @@
+"use strict";
+(() => {
+    console.log('NULL Y UNDEFINED');
+    // En JS, null y undefined son valores permitidos a un string, un booleano...
+    // Pero en las últimas versiones de TS, como estamos trabajando en modo estricto, esto NO es permitido.
+    // undefined
+    let nada = undefined;
+    console.log(nada);
+    // Si el tipo es number, no se puede asignar un undefined a un numérico.
+    // JS lo permite, pero TS nos indica el error.
+    let nadaNumber = undefined;
+    console.log(nadaNumber);
+    // Igual, TS no permite esto, pero JS lo permite sin problemas.
+    let isActive = undefined;
+    console.log(isActive);
+    // Si necesitamos que un booleano pueda valer undefined podemos hacer esto:
+    // Indicarlo explicitamente, que es la mejor solución.
+    let isActive2 = undefined;
+    // O especificar en el fichero tsconfig.json, en la propiedad strictNullChecks, el valor false.
+    // Esto último no es recomendable.
+    // Lo mismo que hemos hablado para undefined vale para null.
+    // PERO null !== undefined
+    // A modo de broma, indicar que en TS, con la propiedad strictNullChecks a false, hay 4 valores permitidos:
+    // true, false, undefined y null
+})();
