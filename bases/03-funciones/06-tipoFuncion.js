5e3d6aabe29cf4d0781e30e14673c4b2e3e51a5f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 15:46:21 +0200

    03-Tipo función

---
+++ bases/03-funciones/06-tipoFuncion.js
@@ -0,0 +1,28 @@
+"use strict";
+(() => {
+    console.log('TIPO FUNCION');
+    const addNumbers = (a, b) => a + b;
+    const greet = (name) => `Hola ${name}`;
+    const saveTheWorld = () => 'El mundo está salvado';
+    // Para permitir cualquier cosa. No hay tipo (es tipo any)
+    //let myFunction;
+    // Para permitir todos los tipos función, usar tipo Function.
+    let myFunction;
+    // Si queremos que el tipo función reciba dos numbers y regrese un número.
+    // let myFunction: (x: number, y: number) => number;
+    // Si queremos que el tipo función reciba un string y regrese un string.
+    // let myFunction: (x: string) => string;
+    //Si queremos que el tipo función no reciba ningún argumento y regrese un string.
+    // let myFunction: () => string;
+    // No permitido
+    //* myFunction = 10;
+    //* console.log(myFunction);
+    // NOTA: En JS todos los objetos pasan por referencia.
+    // TS logra inferir los tipos después de una asignación.
+    myFunction = addNumbers;
+    console.log(myFunction(1, 2));
+    myFunction = greet;
+    console.log(myFunction('José M.'));
+    myFunction = saveTheWorld;
+    console.log(myFunction());
+})();
