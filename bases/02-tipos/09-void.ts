ee893fe4f445e7302da54cd51bf6c2bf04c2a449
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 08:23:30 +0200

    02-tipo void

---
+++ bases/02-tipos/09-void.ts
@@ -0,0 +1,26 @@
+(() => {
+  console.log('VOID');
+
+  // Es bueno indicar, si la función no tiene return que se devuelve void.
+  // Si ahora se indica return <valor>, TS se va a quejar.
+  // Si se indica SOLO return, entonces si es permitido.
+  // Ayuda mucho a la documentación y lectura de las funciones.
+  function callBatman(): void {}
+
+  // Vemos que a tiene el tipo de datos void.
+  // Indica que no hay nada, vacío.
+  // En JS, cuando una función no tiene return, devuelve undefined.
+  // TS lo que hace es indicar que no hay valor de retorno y a ese tipo de datos lo llama void.
+  const a = callBatman();
+  console.log(a); // undefined
+
+  // Para poner tipo de dato devuelto en función de flecha.
+  // NOTA: Se ha puesto return para indicar que es permitido, pero NO haría falta.
+  // return undefined también funciona.
+  const callSuperman = (): void => {
+    return;
+  };
+
+  // NOTAS: null <> void
+  //        null <> undefined
+})();
