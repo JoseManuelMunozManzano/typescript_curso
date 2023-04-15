39efd7db68be3d3710e068b4d52076d555f6d0e1
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-15 06:25:57 +0200

    02-Números

---
+++ bases/02-tipos/03-numbers.js
@@ -0,0 +1,18 @@
+"use strict";
+(() => {
+    console.log('NUMBERS');
+    let avengers; // tipo any solo declarado
+    console.log(avengers); // undefined
+    const villians = 20;
+    // TS nos indica el error de posible undefined.
+    // Este error sería difícil de encontrar porque en JS no salta ningún error (undefined > 20 en JS).
+    if (avengers < villians) {
+        console.log('Estamos en problemas');
+    }
+    else {
+        console.log('Nos salvamos');
+    }
+    // Cuidado con pensar que TS nos da siempre los errores.
+    avengers = Number('55A'); // Devuelve NaN
+    console.log({ avengers }); // Pero NaN es considerado un número y esto funciona!!
+})();
