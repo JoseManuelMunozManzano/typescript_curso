ad4e09e9b5c8388f44ae81bd26f3fa484d8713a8
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-17 20:30:00 +0200

    Funciones Genéricas

--- generics/src/generics/generics.ts
+++ generics/src/generics/generics.ts
@@ -4,7 +4,16 @@ export const printObject = (argument: any) => {
   console.log(argument);
 };
 
-// Función tradicional
-export function genericFunction(argument: any) {
+// Función tradicional.
+// Haciéndola genérica con <T>
+// Estamos indicando que vamos a recibir un tipo de dato cuando se llame a genericFunction,
+// que será el mismo tipo de dato del argumento y por defecto la salida también será del mismo tipo (aunque puede
+// devolver otra cosa si se indica)
+export function genericFunction<T>(argument: T): T {
   return argument;
 }
+
+// Función flecha.
+// Haciéndola genérica.
+// Como se ha indicado antes, si no se indica la salida por defecto es de tipo T.
+export const genericFunctionArrow = <T>(argument: T) => argument;
