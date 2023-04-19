119f6f6e86c3e415c3e9864e46e7613d77e8faa5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-19 06:41:14 +0200

    Decoradores - Intro

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Mis proyectos/apuntes del curso de Fernando Herrera sobre TypeScript.
 - bases
 - ts-node: Hablamos de las distintas formas de importar y exportar y uso de alias.
 - generics
+- decorators
---
+++ decorators/src/index.ts
@@ -0,0 +1,5 @@
+//https://www.typescriptlang.org/docs/handbook/decorators.html
+// Los decoradores son funciones que expanden la funcionalidad de una clase u otra función.
+// Se ejecutan en tiempo de compilación.
+// No suele ser normal crearlos, sino usarlos.
+// Claros exponentes del uso de los decoradores serían los frameworks Angular y Nest.
