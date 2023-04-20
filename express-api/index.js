21e36a409a3de040318c305243fdfb92c3e88cc2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-20 19:48:36 +0200

    Usando librerías que no están escritas en TS. Inicio de proyecto

---
+++ express-api/index.js
@@ -0,0 +1,6 @@
+// Se va a explicar como podemos usar librerías que no fueron escritas en TS y no tenemos soporte del mismo.
+// Ejecutar este archivo js con:
+// node index
+console.log('Hola Mundo!');
+
+// Primero vamos a ver como haríamos un Rest API con Express en JS y luego lo pasaremos a TS
