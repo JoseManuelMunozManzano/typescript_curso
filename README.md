9aa8b8b3c1ff3700e27eaf4424765027025b1f12
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-20 19:57:33 +0200

    Creando un Rest API con Express en JS

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Mis proyectos/apuntes del curso de Fernando Herrera sobre TypeScript.
 
 - bases
-- ts-node: Hablamos de las distintas formas de importar y exportar y uso de alias.
+- ts-node: Hablamos de las distintas formas de importar y exportar y uso de alias
 - generics
 - decorators
+- Usando librerías que no están escritas en TypeScript
--- express-api/index.js
+++ express-api/index.js
@@ -1,6 +1,26 @@
 // Se va a explicar como podemos usar librerías que no fueron escritas en TS y no tenemos soporte del mismo.
-// Ejecutar este archivo js con:
-// node index
-console.log('Hola Mundo!');
 
 // Primero vamos a ver como haríamos un Rest API con Express en JS y luego lo pasaremos a TS
+
+// https://expressjs.com/
+// Para instalar:
+// npm i express
+// Express por defecto no tiene soporte de TS.
+//
+// Ejecutar con el mandato:
+// npm start
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!');
+  res.json({
+    ok: true,
+    msg: 'Todo salio bien',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
